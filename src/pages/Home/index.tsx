a2afd341b01b7c109e782b231426835b1e301f2d
Vivian Desktop <visasal88@gmail.com>
2024-02-06 15:44:28 -0300

    initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export function Home() {
+
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  )
+}
+
---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Search() {
+
+  return (
+    <>
+      <h1>Search page</h1>
+    </>
+  )
+}
